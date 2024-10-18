e1f9a8b74aec13e0c11c870dccf962ce421ec9a9
Duda Mello <147108805+Dudamelloooo@users.noreply.github.com>
2024-10-18 19:00:24 -0300

    NAO SEIIIIIIIIIIIIIII

--- viwer/controller/script.js
+++ viwer/controller/script.js
@@ -17,3 +17,15 @@ orderButtons.forEach((button) => {
     window.open(whatsappLink, '_blank');
   });
 });
+
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
