db761863a29638892d94fb4e706e45d49be5b163
Duda Mello <147108805+Dudamelloooo@users.noreply.github.com>
2024-10-18 18:48:24 -0300

    aruumei o js e os cards

--- viwer/controller/script.js
+++ viwer/controller/script.js
@@ -1,50 +1,19 @@
-const colorButtons = document.querySelectorAll('.color-btn');
-const genderButtons = document.querySelectorAll('.gender-btn');
-const productImage = document.getElementById('productImg');
-const orderButton = document.getElementById('orderBtn');
-const sizeSelect = document.getElementById('sizes');
-let selectedColor = 'red'; // Cor padrão
-let selectedGender = 'masculino'; // Gênero padrão
 
-// Função para alterar a imagem com base na cor e gênero selecionados
-function updateProductImage() {
-  productImage.src = `product-${selectedGender}-${selectedColor}.jpg`;
-}
+const orderButtons = document.querySelectorAll('#orderBtn');
 
-// Event listeners para seleção de cores
-colorButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    selectedColor = button.getAttribute('data-color');
-    updateProductImage(); // Atualizar imagem quando a cor mudar
-  });
-});
+orderButtons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    event.preventDefault();
+    
+    const productName = button.closest('.product-details').querySelector('.product-title').textContent;
+    const productPrice = button.closest('.product-details').querySelector('.product-price').textContent;
 
-// Event listeners para seleção de gênero
-genderButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    selectedGender = button.getAttribute('data-gender');
-    updateProductImage(); // Atualizar imagem quando o gênero mudar
-  });
-});
-
-// Função para enviar mensagem via WhatsApp com os detalhes do pedido
-orderButton.addEventListener('click', () => {
-  const selectedSize = sizeSelect.value;
-  const productName = document.querySelector('.product-title').innerText;
-  const productPrice = document.querySelector('.product-price').innerText;
-
-  // Criar mensagem de WhatsApp
-  const message = `Olá,Bom dia! Tudo bem? Estou com interesse no:
+    const message = `Olá, bom dia! Tudo bem? Quais cores e tamanhos vocês têm disponível para:
 - Nome: ${productName}
-- Tamanho: ${selectedSize}
-- Gênero: ${selectedGender}
-- Cor: ${selectedColor}
 - Preço: ${productPrice}`;
 
-  // WhatsApp API link com a mensagem codificada
-  const whatsappLink = `https://wa.me/5541992151906?text=${encodeURIComponent(message)}`;
+    const whatsappLink = `https://api.whatsapp.com/send?phone=5541992017347&text=${encodeURIComponent(message)}`;
 
-  // Redirecionar para o WhatsApp
-  window.open(whatsappLink, '_blank');
+    window.open(whatsappLink, '_blank');
+  });
 });
-
