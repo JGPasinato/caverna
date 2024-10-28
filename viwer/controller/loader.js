a07e4f4f75dc259960ce378923bdff9e2b01a693
João Pasinato <joaogabimelo@gmail.com>
2024-10-27 21:53:21 -0300

    link

--- viwer/controller/loader.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var loader = document.getElementById("loader");
-    loader.style.display = "none";
-});
-
-window.addEventListener("load", function() {
-    var loader = document.getElementById("loader");
-    loader.style.display = "none";
-});
